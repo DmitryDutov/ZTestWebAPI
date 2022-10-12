1a7fa0a20e8103e1dcb3c66700bb59ab84c8396e
DmitryDutov <dmitry_dutov@inbox.ru>
2022-10-12 15:10:36 +0300

    Создан словарь, поля, конструктор и Get-запрос

--- ZTestWebAPI/Controllers/ValuesController.cs
+++ ZTestWebAPI/Controllers/ValuesController.cs
@@ -6,7 +6,23 @@ namespace ZTestWebAPI.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly ILogger<ValuesController> _logger;
 
+        //Получаем сервисы в конструктор и передаём их в поля класса
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        //Создаём нумерованный словарь и заполняем его
+        private static readonly Dictionary<int, string> Values = Enumerable
+            .Range(1, 10)
+            .Select(i => (Id: i, Value: $"Value-{i}"))
+            .ToDictionary(v => v.Id, v => v.Value);
+
+        //Создадим набор действий в для управления словарём
+        [HttpGet]
+        public IActionResult Get() => Ok(Values.Values);
     }
 }
 
