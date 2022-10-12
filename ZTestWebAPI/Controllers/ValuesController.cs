b0ef27399e6963f6c9b9deac66056c919eeb00f5
DmitryDutov <dmitry_dutov@inbox.ru>
2022-10-12 16:03:10 +0300

    Добавлен Replace и Delete

--- ZTestWebAPI/Controllers/ValuesController.cs
+++ ZTestWebAPI/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace ZTestWebAPI.Controllers
             .Select(i => (Id: i, Value: $"Value-{i}"))
             .ToDictionary(v => v.Id, v => v.Value);
 
-        //Создадим набор действий в для управления словарём (получение всего набора, получение значения по Id, подсчёт количества, редактировать, удалить значение по Id)
+        //Создадим набор действий в для управления словарём (получение всего набора, получение значения по Id, подсчёт количества, добавить, редактировать, удалить значение по Id)
         [HttpGet] //GET -> http://localhost:5000/api/values
         public IActionResult Get() => Ok(Values.Values);
 
@@ -38,7 +38,7 @@ namespace ZTestWebAPI.Controllers
         [HttpGet("count")] //GET -> http://localhost:5000/api/values/count
         //public IActionResult Count() => Ok(Values.Count);
         //public ActionResult<int> Count()=> Values.Count();
-        public int Count()=>Values.Count;
+        public int Count() => Values.Count;
 
         [HttpPost] //POST -> http://localhost:5000/api/values
         [HttpPost("add")] //POST -> http://localhost:5000/api/values/add
@@ -47,7 +47,33 @@ namespace ZTestWebAPI.Controllers
             var id = Values.Count == 0 ? 1 : Values.Keys.Max() + 1;
             Values[id] = Value;
 
-            return CreatedAtAction(nameof(GetById), new {id});
+            return CreatedAtAction(nameof(GetById), new { id });
+        }
+
+        [HttpPut("{Id}")] //PUT -> http://localhost:5000/api/values/5
+        public IActionResult Repalace(int Id, [FromBody] string Value)
+        {
+            if (!Values.ContainsKey(Id))
+            {
+                return NotFound();
+            }
+
+            Values[Id] = Value;
+
+            return Ok();
+        }
+
+        [HttpDelete] //DELETE -> http://localhost:5000/api/values/5
+        public IActionResult Delete(int Id)
+        {
+            if (!Values.ContainsKey(Id))
+            {
+                return NotFound();
+            }
+
+            Values.Remove(Id);
+
+            return Ok();
         }
     }
 }
