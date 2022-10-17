195fc01d3724000f4cc7d0cf8223a3f4623a74c3
DmitryDutov <dmitry_dutov@inbox.ru>
2022-10-17 12:34:18 +0300

    Добавлен и настроен первичный проект Blazor

--- ZTestWebAPI/Program.cs
+++ ZTestWebAPI/Program.cs
@@ -10,11 +10,15 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseWebAssemblyDebugging(); //Подключение к браузеру для отладки Blazor
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseBlazorFrameworkFiles();       //Добавляем поддержку инфраструктуры Blazor
+app.UseStaticFiles();                //Статические файлы нужны для работы стилей и скриптов
 app.UseAuthorization();
 app.MapControllers();
+app.MapFallbackToFile("index.html"); //Если адрес непонятен, то перенаправляем на приложение Blazor
 app.Run();
 
